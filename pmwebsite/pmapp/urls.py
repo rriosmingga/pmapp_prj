50a161a72e1d275e540117a3749ad48d8bf40bff
rriosmingga <rene@minggalabs.com>
2015-08-12 22:23:52 -0300

    PMWS: vistas

--- pmwebsite/pmapp/urls.py
+++ pmwebsite/pmapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^equipo/(?P<equipo_id>[0-9]+)/$', views.index, name='teamdetail'),
-    url(r'^partido/(?P<partido_id>[0-9]+)/$', views.index, name='matchdetail'),
+    url(r'^equipo/(?P<equipo_id>[0-9]+)/$', views.teamdetail, name='teamdetail'),
+    url(r'^partido/(?P<partido_id>[0-9]+)/$', views.matchdetail, name='matchdetail'),
 ]
--- pmwebsite/pmapp/views.py
+++ pmwebsite/pmapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from pmapp.models import Equipo, Partido
 
 
 def index(request):
-    lista_partidos = Partido.objects.order_by('-pk')[:2]
+    lista_partidos = Partido.objects.order_by('-pk')[:10]
     context = {'lista_partidos' : lista_partidos}
     return render(request, 'pmapp/index.html', context)
+    
+def teamdetail(request, equipo_id):
+	response = "Estas en la pagina del equipo %s."
+	return HttpResponse(response % equipo_id)
+	
+def matchdetail(request, partido_id):
+	response = "Estas en la pagina del partido %s."
+	return HttpResponse(response % partido_id)
