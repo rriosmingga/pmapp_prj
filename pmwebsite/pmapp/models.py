65315258640d64e4836e7a39148e6d4859852a63
rriosmingga <rene@minggalabs.com>
2015-08-12 21:29:04 -0300

    PMWS: crea modelos y settings

--- pmwebsite/pmapp/models.py
+++ pmwebsite/pmapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Equipo(models.Model):
+	name_text = models.CharField(max_length=50)
+	def __str__(self):
+		return self.name_text
+		
+class Partido(models.Model):
+    team1 = models.ForeignKey(Equipo, related_name='local')
+    team2 = models.ForeignKey(Equipo, related_name='visit')
+    def __str__(self):
+        return "%s vs %s" % (self.team1, self.team2)
+	
